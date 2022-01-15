2420c837c962f1352e8d9445d09849c2bd9988b8
MV Phaneendra <mvphaneendra@gmail.com>
2022-01-15 20:30:10 +0530

    first commit

---
+++ lib/app_routes.dart
@@ -0,0 +1,18 @@
+
+import 'package:dietapp_a/auth_gate.dart';
+import 'package:get/get.dart';
+
+class AppRoutes {
+  //static const initial = GetPage(name: "/", page: ()=>HomeScreen2())
+  static final routes = [
+    GetPage(
+      name: "/",
+      page: () => AuthGate(),
+      //binding: HomeBinding(),
+    ),
+    // // Login
+    // GetPage(
+    //     name: "/loginScreen", page: () => LoginView(), binding: LoginBinding()),
+    // Welcome
+  ];
+}
---
+++ lib/auth_gate.dart
@@ -0,0 +1,35 @@
+import 'package:dietapp_a/0-homeScreen/0_home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        return StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            // User is not signed in
+            if (!snapshot.hasData) {
+              return const SignInScreen(
+                providerConfigs: [
+                  GoogleProviderConfiguration(
+                    clientId: '1:237115759240:android:2ce36ff5e9acc35bfc9d3d',
+                  ),
+                ],
+              );
+            }
+
+            // Render your application if authenticated
+            return HomeScreen();
+          },
+        );
+      },
+    );
+  }
+}
